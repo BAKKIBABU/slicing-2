f3d4e7f7133095a882946cd38608865799a4b0be
BAKKIBABU <108388125+BAKKIBABU@users.noreply.github.com>
2022-07-04 15:40:40 +0530

    Add files via upload

---
+++ slicing2.py
@@ -0,0 +1,43 @@
+# a='python'
+# print(a[4:])
+# print(a[5:])
+# print(a[:1])
+# print(a[:2])
+# print(a[2:5])  # tho
+# print(a[-1])
+# print(a[-5])
+# print(a[-3])
+# print(a[-4])
+# print(a[:-1])
+# print(a[:-6])
+# print(a[:])
+# print(a[:-1])
+# print(a[:-8]) #blonkline
+
+# a='1234'
+# print(a[0]+[-1])
+# print(int(a[0])+int(a[-1]))
+# '''greeper chrome extension'''
+# a='python'
+# print(a[::-1])
+
+# c='python'
+# print(c[: : 1])
+
+# c='python'
+# print(c[ : : 2])  #pto
+# print(c[ : : 1])
+# print(a[ 0: 5:5])
+# print(a[ :3 :5])
+# print(a[ : 5:3])
+# print(a[ 1: :5])
+# print(a[ 2: :2])
+# print(a[ 1: :2])
+
+# d = 'python'
+# print(d[ :5 :])
+# print(d[ :5 :1])
+# print(d[5 : :5])
+# print(d[ 1: 4 :2])
+# print(d[ 4:3 :1])  #blonkline
+# print(d[ 5:2 :1])   #blonkline
